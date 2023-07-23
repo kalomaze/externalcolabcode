1b847a5b9a051b51e8dc78c980515865ee1ca160
kalomaze <66376113+kalomaze@users.noreply.github.com>
2023-07-23 01:42:43 -0500

    Implement concurrent futures + reduce metadata access

--- backups_test.py
+++ backups_test.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import hashlib
 import time
+import concurrent.futures
 
 LOGS_FOLDER = '/content/Retrieval-based-Voice-Conversion-WebUI/logs'
 WEIGHTS_FOLDER = '/content/Retrieval-based-Voice-Conversion-WebUI/weights'
@@ -17,25 +18,38 @@ def import_google_drive_backup():
     files_to_copy = []
     weights_to_copy = []
 
+    existing_dirs = set()
+
     for root, dirs, files in os.walk(GOOGLE_DRIVE_PATH):
         for filename in files:
             filepath = os.path.join(root, filename)
             if os.path.isfile(filepath) and not filepath.startswith(os.path.join(GOOGLE_DRIVE_PATH, 'weights')):
                 backup_filepath = os.path.join(LOGS_FOLDER, os.path.relpath(filepath, GOOGLE_DRIVE_PATH))
                 backup_folderpath = os.path.dirname(backup_filepath)
-                if not os.path.exists(backup_folderpath):
+                if backup_folderpath not in existing_dirs and not os.path.exists(backup_folderpath):
                     os.makedirs(backup_folderpath)
+                    existing_dirs.add(backup_folderpath)
                     print(f'Created backup folder: {backup_folderpath}', flush=True)
                 files_to_copy.append((filepath, backup_filepath))  # add to list of files to copy
             elif filepath.startswith(os.path.join(GOOGLE_DRIVE_PATH, 'weights')) and filename.endswith('.pth'):
                 weights_exist = True
                 weights_filepath = os.path.join(WEIGHTS_FOLDER, os.path.relpath(filepath, os.path.join(GOOGLE_DRIVE_PATH, 'weights')))
                 weights_folderpath = os.path.dirname(weights_filepath)
-                if not os.path.exists(weights_folderpath):
+                if weights_folderpath not in existing_dirs and not os.path.exists(weights_folderpath):
                     os.makedirs(weights_folderpath)
+                    existing_dirs.add(weights_folderpath)
                     print(f'Created weights folder: {weights_folderpath}', flush=True)
                 weights_to_copy.append((filepath, weights_filepath))  # add to list of weights to copy
 
+    # parallel copying files and weights
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # copy other files
+        for source, target in files_to_copy:
+            executor.submit(shutil.copy2, source, target)
+
+        # copy weights files
+        for source, target in weights_to_copy:
+            executor.submit(shutil.copy2, source, target)
     # Copy files in batches
     total_files = len(files_to_copy)
     for i, (source, dest) in enumerate(files_to_copy, start=1):
