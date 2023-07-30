c2ea8ef82f328b022f2753ce71b6d15defb46792
kalomaze <66376113+kalomaze@users.noreply.github.com>
2023-07-30 06:59:28 -0500

    experimental colab tweaked

--- clonerepo_experimental.py
+++ clonerepo_experimental.py
@@ -115,7 +115,7 @@ def run_script():
 
     def clone_and_copy_repo(repo_path):
         # New repository link
-        new_repo_link = "https://github.com/alexlnkp/Mangio-RVC-Tweaks.git"
+        new_repo_link = "https://github.com/alexlnkp/Mangio-RVC-Tweaks/"
         # Temporary path to clone the repository
         temp_repo_path = "/content/temp_Mangio-RVC-Fork"
         # New folder name
@@ -125,6 +125,7 @@ def run_script():
         run_cmd(f"git clone {new_repo_link} {temp_repo_path}")
         os.chdir(temp_repo_path)
 
+        run_cmd(f"git checkout pr-optimization")
         run_cmd("wget https://cdn.discordapp.com/attachments/1125223511191990333/1134555255653285908/EasierGUI.py")
 
         # Edit the file here, before copying
