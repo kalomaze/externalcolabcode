fa920601ff73c3bf9bb4db354ef41ffab3350c3b
Sơn Phan Trung <94152483+sonphantrung@users.noreply.github.com>
2023-08-31 19:53:34 +0700

    Update backups_test.py

--- backups_test.py
+++ backups_test.py
@@ -4,15 +4,15 @@ import shutil
 import hashlib
 import time
 
-LOGS_FOLDER = '/content/Retrieval-based-Voice-Conversion-WebUI/logs'
-WEIGHTS_FOLDER = '/content/Retrieval-based-Voice-Conversion-WebUI/weights'
+LOGS_FOLDER = '/content/Mangio-RVC-Tweaks/logs'
+WEIGHTS_FOLDER = '/content/Mangio-RVC-Tweaks/weights'
 GOOGLE_DRIVE_PATH = '/content/drive/MyDrive/RVC_Backup'
 
 def import_google_drive_backup():
     print("Importing Google Drive backup...")
     GOOGLE_DRIVE_PATH = '/content/drive/MyDrive/RVC_Backup'  # change this to your Google Drive path
-    LOGS_FOLDER = '/content/Retrieval-based-Voice-Conversion-WebUI/logs'
-    WEIGHTS_FOLDER = '/content/Retrieval-based-Voice-Conversion-WebUI/weights'
+    LOGS_FOLDER = '/content/Mangio-RVC-Tweaks/logs'
+    WEIGHTS_FOLDER = '/content/Mangio-RVC-Tweaks/weights'
     weights_exist = False
     files_to_copy = []
     weights_to_copy = []
